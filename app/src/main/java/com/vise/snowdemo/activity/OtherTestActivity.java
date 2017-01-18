32eebbd3a0b934fb642de7035c5aa384fa5201bb
xyy <553252628@qq.com>
2017-01-19 00:13:53 +0800

    update commit

---
+++ app/src/main/java/com/vise/snowdemo/activity/OtherTestActivity.java
@@ -0,0 +1,34 @@
+package com.vise.snowdemo.activity;
+
+import android.os.Bundle;
+
+import com.vise.snowdemo.R;
+import com.vise.xsnow.ui.BaseActivity;
+
+/**
+ * @Description:
+ * @author: <a href="http://www.xiaoyaoyou1212.com">DAWI</a>
+ * @date: 17/1/18 23:10.
+ */
+public class OtherTestActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_test);
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void bindEvent() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
