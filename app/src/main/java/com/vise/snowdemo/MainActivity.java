9df2599642426ab8c307d62cea81408cbf55c53c
HET <553252628@qq.com>
2017-01-19 20:16:22 +0800

    update commit

--- app/src/main/java/com/vise/snowdemo/MainActivity.java
+++ app/src/main/java/com/vise/snowdemo/MainActivity.java
@@ -1,19 +1,24 @@
 package com.vise.snowdemo;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import com.vise.snowdemo.activity.DownTestActivity;
 import com.vise.snowdemo.activity.ImageLoaderActivity;
+import com.vise.snowdemo.activity.NetTestActivity;
+import com.vise.snowdemo.activity.OtherTestActivity;
 import com.vise.utils.view.ActivityUtil;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -29,14 +34,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                displayAboutDialog();
             }
         });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -52,51 +52,44 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.menu_net) {
+            ActivityUtil.startForwardActivity(this, NetTestActivity.class);
+        } else if (id == R.id.menu_down) {
+            ActivityUtil.startForwardActivity(this, DownTestActivity.class);
+        } else if (id == R.id.menu_image_loader) {
             ActivityUtil.startForwardActivity(this, ImageLoaderActivity.class);
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.menu_other) {
+            ActivityUtil.startForwardActivity(this, OtherTestActivity.class);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void displayAboutDialog() {
+        final int paddingSizeDp = 5;
+        final float scale = getResources().getDisplayMetrics().density;
+        final int dpAsPixels = (int) (paddingSizeDp * scale + 0.5f);
+
+        final TextView textView = new TextView(this);
+        final SpannableString text = new SpannableString(getString(R.string.about_dialog_text));
+
+        textView.setText(text);
+        textView.setAutoLinkMask(RESULT_OK);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setPadding(dpAsPixels, dpAsPixels, dpAsPixels, dpAsPixels);
+
+        Linkify.addLinks(text, Linkify.ALL);
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.menu_about)
+                .setCancelable(false)
+                .setPositiveButton(android.R.string.ok, null)
+                .setView(textView)
+                .show();
     }
 }
