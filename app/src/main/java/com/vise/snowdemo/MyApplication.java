d04c862d9399b928facaf34829afce95fd7f9037
xyy <553252628@qq.com>
2017-06-25 20:14:54 +0800

    增加网络全局配置介绍

--- app/src/main/java/com/vise/snowdemo/MyApplication.java
+++ app/src/main/java/com/vise/snowdemo/MyApplication.java
@@ -4,10 +4,26 @@ import com.vise.log.ViseLog;
 import com.vise.log.inner.LogcatTree;
 import com.vise.netexpand.convert.GsonConverterFactory;
 import com.vise.snowdemo.db.DbHelper;
+import com.vise.utils.assist.SSLUtil;
 import com.vise.xsnow.BaseApplication;
+import com.vise.xsnow.common.ViseConfig;
 import com.vise.xsnow.http.ViseHttp;
+import com.vise.xsnow.http.core.ApiCookie;
 import com.vise.xsnow.http.interceptor.HttpLogInterceptor;
+import com.vise.xsnow.http.interceptor.NoCacheInterceptor;
 import com.vise.xsnow.loader.LoaderFactory;
+
+import java.io.File;
+import java.net.Proxy;
+import java.net.SocketAddress;
+import java.util.HashMap;
+
+import okhttp3.Cache;
+import okhttp3.Call;
+import okhttp3.ConnectionPool;
+import okhttp3.Request;
+import okhttp3.internal.cache.CacheInterceptor;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 /**
  * @Description: 自定义Application，主要负责一些初始化操作
@@ -34,11 +50,64 @@ public class MyApplication extends BaseApplication {
     private void initNet() {
         ViseHttp.init(this);
         ViseHttp.CONFIG()
-                .baseUrl("http://10.8.4.39/")
+                //配置请求主机地址
+                .baseUrl("http://192.168.1.100/")
+                //配置全局请求头
+                .globalHeaders(new HashMap<String, String>())
+                //配置全局请求参数
+                .globalParams(new HashMap<String, String>())
+                //配置读取超时时间，单位秒
+                .readTimeout(30)
+                //配置写入超时时间，单位秒
+                .writeTimeout(30)
+                //配置连接超时时间，单位秒
+                .connectTimeout(30)
+                //配置请求失败重试次数
+                .retryCount(3)
+                //配置请求失败重试间隔时间，单位毫秒
+                .retryDelayMillis(1000)
+                //配置是否使用cookie
                 .setCookie(true)
-                .converterFactory(GsonConverterFactory.create())
+                //配置自定义cookie
+//                .apiCookie(new ApiCookie(this))
+                //配置是否使用OkHttp的默认缓存
+                .setHttpCache(true)
+                //配置OkHttp缓存路径
+//                .setHttpCacheDirectory(new File(ViseHttp.getContext().getCacheDir(), ViseConfig.CACHE_HTTP_DIR))
+                //配置自定义OkHttp缓存
+//                .httpCache(new Cache(new File(ViseHttp.getContext().getCacheDir(), ViseConfig.CACHE_HTTP_DIR), ViseConfig.CACHE_MAX_SIZE))
+                //配置自定义离线缓存
+//                .cacheOffline(new Cache(new File(ViseHttp.getContext().getCacheDir(), ViseConfig.CACHE_HTTP_DIR), ViseConfig.CACHE_MAX_SIZE))
+                //配置自定义在线缓存
+//                .cacheOnline(new Cache(new File(ViseHttp.getContext().getCacheDir(), ViseConfig.CACHE_HTTP_DIR), ViseConfig.CACHE_MAX_SIZE))
+                //配置开启Gzip请求方式，需要服务器支持
+//                .postGzipInterceptor()
+                //配置应用级拦截器
                 .interceptor(new HttpLogInterceptor()
-                        .setLevel(HttpLogInterceptor.Level.BODY));
+                        .setLevel(HttpLogInterceptor.Level.BODY))
+                //配置网络拦截器
+//                .networkInterceptor(new NoCacheInterceptor())
+                //配置转换工厂
+                .converterFactory(GsonConverterFactory.create())
+                //配置适配器工厂
+                .callAdapterFactory(RxJava2CallAdapterFactory.create())
+                //配置请求工厂
+//                .callFactory(new Call.Factory() {
+//                    @Override
+//                    public Call newCall(Request request) {
+//                        return null;
+//                    }
+//                })
+                //配置连接池
+//                .connectionPool(new ConnectionPool())
+                //配置主机证书验证
+                .hostnameVerifier(new SSLUtil.UnSafeHostnameVerifier("http://192.168.1.100/"))
+                //配置SSL证书验证
+                .SSLSocketFactory(SSLUtil.getSslSocketFactory(null, null, null))
+                //配置主机代理
+//                .proxy(new Proxy(Proxy.Type.HTTP, new SocketAddress() {}))
+                ;
+
     }
 
 }
