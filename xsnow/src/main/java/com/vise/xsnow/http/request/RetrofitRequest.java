7832822d57fec3558828404c6a1403444c242cf3
xyy <553252628@qq.com>
2017-07-23 00:49:04 +0800

    2.1.0版本发布

--- xsnow/src/main/java/com/vise/xsnow/http/request/RetrofitRequest.java
+++ xsnow/src/main/java/com/vise/xsnow/http/request/RetrofitRequest.java
@@ -1,7 +1,7 @@
 package com.vise.xsnow.http.request;
 
 /**
- * @Description:
+ * @Description: 传入自定义Retrofit接口的请求类型
  * @author: <a href="http://www.xiaoyaoyou1212.com">DAWI</a>
  * @date: 17/7/22 15:11.
  */
